a165a66ef117f754ae5cab712db09ab512008673
yazidvr <90797910+yazidvr@users.noreply.github.com>
2021-12-03 12:52:14 -0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,49 @@
+#RUSSIAN FAMILY  !         
+#Tool For Hacking 
+#Authors : YAZID
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/Crips/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/Manisso
+
+clear
+
+mkdir Manisso
+
+clear
+
+cd Manisso
+
+clear
+
+git clone https://github.com/Manisso/Crips.git
+
+clear
+
+cd Crips
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
